86f36777594b296078ef68a65c547cc55f218cdf
John Lee <johndlee2009@gmail.com>
2022-03-25 23:06:50 -0400

    Initial Commite (utils.py is incomplete)

---
+++ Final Project/README.txt
@@ -0,0 +1,84 @@
+install requirements by running
+
+% pip install -r requirements.txt
+
+
+
+
+
+Notes for Project
+
+Steps:
+    1. Retrieve/Store Data
+        - ID/Secretkey
+            - PKF1UA7IOK2GNIOZJCCE
+            - qoUq6tjPOwpwDld6DfjTvov5q0bAj0KvvHnUSUDx
+        - Stocks
+            - Tech
+                - TSLA, AAPL, NVDA
+            - Finance
+                - GS, V, JPM
+ 
+    2. Clean Data
+        - Remove AH (easy) 
+            - Compute returns before removing
+            - Make assumption that all valid trading days begins at 9:30-4:00 
+                - Data will be kept from 9 - 4
+        - Ignore gaps (they won't play a role in calculation anyways)
+        - Drop nan values
+        - split data in to train & test = 50/50.
+            - this is because we only predict 1 timestep in the future and factor the data into p after that time step passes
+
+    3. Control Model
+        - Bear/Bull matrix
+            - Bulls >= 0
+            - Bears < 0
+        - Compute Probability on test
+        - Compute new probs over all time periods
+
+    4. Other Models
+        1. Thresholding
+        2. Pairing
+        3. P(Dt, Ht)
+        4. Combination etc.
+    
+    5. "Train"
+        - Compute initial Probability (whether it is constant or not)
+        - Compute initial means and variances
+
+    6. "Test"
+        - For each state (in matrix like P), track mean returns and std returns. 
+        - For every month in test, simulate n runs, with the predicted return being a normal distribution of the selected state (due to MC).
+        - Add new model to Training set
+
+    7. Statistics
+        - Strategy based computations  ( Computed over each test period AND can aggregate a mean/median P/L. For over all, compute a mean/median/max/min P/L)
+            - P/L
+            - Sharpe
+        - Computed on predicted model (These hold constant regardless of strategy) (Compute B and Corr on each run and then compute quantiles over all runs) (Computed over each test period)
+            - Correlation
+            - SMAPE
+
+        - Plots
+            Like Paper
+                - Sections [train, test1, test2 ...]
+                - train -> only reals
+                - testn -> real and our predictions
+                - Can split with red line.
+
+            Quantile of simulations
+                - Computed by quantiles of P/L
+                - Max & min & median 
+            
+            Box plot/Histogram 
+                - For Strategy based
+                    - Straightforward
+                - For Corr/Smape
+                    - Boxplot/bargraph mix
+
+    8. Plausible Strategy
+        - Profit target = X% of max value in sim
+        - Stop target = Min value in sim - (some percent/value)
+
+
+    tkomeiji20
---
+++ Final Project/utils.py
@@ -0,0 +1,187 @@
+import pandas as pd
+import numpy as np
+from tqdm import tqdm, trange
+import os
+
+class BaseSim:
+    
+    def __init__(self, states: list):
+        
+        # set up states
+        self.states = list(states)
+        # initialize P
+        self.P = dict(zip(list(states), [dict(zip(list(states), [-1 for i in range(len(states))])) for i in range(len(states))]))
+        self.M = dict(zip(list(states), [ 0 for i in range(len(states))]))
+        self.STD = dict(zip(list(states), [ 0 for i in range(len(states))]))
+        
+        self.strategies = ['B&H'] # only B&H for now
+        
+    def reset(self):
+        self.__init__(self.states)
+    ####################
+    # Simulation Fuction
+    ####################
+    
+    def run_simulation(self, runs: int, data: pd.DataFrame, ret_colname = 'returns', split: list = [.5, .5], pred_period = 140):
+        ''' Runs entire simulation and saves metrics
+        Data Provided should already cleaned and ready to use. Must have a minimum of periodic returns as a column
+        compute the start state of the simulation.
+        
+        Returns:
+            Simulation data as a numpy, where each time period is [predicted, true]'''
+        
+        self.ret_colname = ret_colname
+        
+        # split data into train and test
+        train_len = split[0] * len(data)
+        train_data = data[:train_len].copy()
+        test_data = data[train_len:].copy()
+        
+        self.train(train_data)
+        
+        runs = []
+        # Monte Carlo Simulation
+        for run in trange(runs, desc = 'Runs Completed'):
+            
+            sim = []
+            testn = []
+            cur_state = self.compute_startstate(test_data)
+            # iterate through test data
+            for time_step in trange(len(test_data), desc = 'Timestep'):
+                
+                # every pred_period of data, add data to test data
+                if time_step % pred_period == 0 and time_step != 0:
+                    
+                    # if it isn't first period, append the previous 140 and remove from test
+                    train_data.append(test_data[time_step-pred_period:time_step].copy())
+                    
+                    # save pred_periods worth of data into an array
+                    sim.append(testn)
+                    testn = [] # reset testn
+                    
+                    # retrain
+                    self.train(train_data)
+                    
+                    
+                # modifieable test_step
+                self.test_step(test_data)
+                
+                # compute a return using normal distribution and save it w/ true value
+                testn.append([np.random.normal(loc = self.M[cur_state], scale = self.STD[cur_state]), test_data[time_step][self.ret_colname]] )
+                
+                # select the next state
+                prob = np.random.uniform(0, 1)
+                state_prob = 0
+                for state in self.states:
+                    state_prob += self.P[cur_state][state]
+                    if prob < state_prob:
+                        cur_state = state # set the next state
+                        break
+            # capture last piece of data
+            sim.append(testn)
+            # save to our runs
+            runs.append(sim) 
+        
+        del self.ret_colname
+        
+        return np.array(runs)
+
+    ###########################
+    # Custom Functions to be defined in child classes
+    ###########################
+    
+    def compute_startstate(self, test_data: pd.DataFrame):
+        """Modify this step to compute the start state for the simulation. You are given the test_data. Ensure consistent column names when referencing or use self.ret_colname
+        
+        Return the start state"""
+        pass
+    
+    def train(self, training_data: pd.DataFrame):
+        """Modify this step to initially fill self.P with transition probs, self.M with mean returns, and self.STD with std of returns for each state.
+        
+        You are given the entire set of training data. Ensure when referencing training data, you use consistent column names or use self.ret_colname.
+        
+        Do not Return"""
+        pass
+    
+    def test_step(self, test_data: pd.DataFrame):
+        """Modify this test step to change P or perform other operations if necessary before computing return for that time period
+        
+        Do Not Return"""
+        pass
+    
+
+    #########################
+    # Statistic functions
+    #########################
+    def get_strategies(self):
+        print('Valid Strategies:')
+        for strat in self.strategies:
+            print(f'\t{strat}')
+    
+    def compute_metrics(self, run_data: np.array, strategy = 'B&H'):
+        '''Should feed in simulation data after running. Will compute P/L (strategy), Sharpe, Loss
+        run_data:
+            Outer Layer = 0 - runs # of simulations
+            Layer 2 = 0 - # of test periods
+            Layer 3 = 0 - # of time periods per test period
+            Layer 4 = [predicted, true],
+            
+        returns a dictionary of computed strategies'''
+            
+        pnl = self.PnL(run_data, strategy)
+        sharpe = self.Sharpe(run_data, pnl)
+        corr = self.corr(run_data)
+        smape = self.SMAPE(run_data)
+        
+        return {'PnL': pnl, 'Sharpe': sharpe, 'Correlation': corr, 'SMAPE': smape}
+        
+        
+        
+    def PnL(self, run_data: np.array, strategy = 'B&H'):
+        """Compute P/L on run_data with strategy.
+        
+        Return a 2-d array of where:
+            Outer Layer = 0 - runs # of simulations
+            Layer 2 = 0 - # of test periods P/L calculations
+            """
+        return
+
+    def Sharpe(self, run_data: np.array, PnL_data: np.array):
+        """ Computes sharpe using run_data and data computed from PnL,
+            Return a 2-d array of where:
+                Outer Layer = 0 - runs # of simulations
+                Layer 2 = 0 - # of test periods Sharpe calculations
+            
+        """
+        return
+    
+    def corr(self, run_data: np.array):
+        """ Computes correlation of predicted model to true data
+            Return a 2-d array of where:
+                Outer Layer = 0 - runs # of simulations
+                Layer 2 = 0 - # of test periods Corr calculations"""
+        return
+
+    def SMAPE(self, run_data: np.array):
+        """ Computes correlation of predicted model to true data
+            Return a 2-d array of where:
+                Outer Layer = 0 - runs # of simulations
+                Layer 2 = 0 - # of test periods SMAPE calculations"""
+        return
+    
+
+    #########################
+    # Save Functions
+    #########################
+    
+    def save_sim(self, run_data, filepath):
+        """ Saves Run data as an npy binary file"""
+        np.save(filepath, run_data, fix_imports=False)
+        return
+    
+    def save_metrics(self, metrics:dict, filepath):
+        """ Saves metrics in a npz zip file"""
+        np.savez(filepath, **metrics)
+        return
+        
